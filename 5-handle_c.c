8b7b5ec81d8ea33849e2d639fcd22b0dd6ef3099
Mohamed Badr <mouhamedebrahim1@outlook.com>
2024-09-07 13:46:24 -0700

    implement code to handle type 'c'

--- 5-handle_c.c
+++ 5-handle_c.c
@@ -1,8 +1,7 @@
 #include "main.h"
-int check_buf(char *buf, int *buf_index);
 
 /**
- * handle_i - Handle the type conversion `i` or `d` (integer values/numbers).
+ * handle_c - Handle the type conversion `c` (character values).
  * @buf: A pointer the buffer where the formatted output should be stored.
  * @buf_index: A pointer to the current index in the buffer.
  * @fs: A pointer to `fs_t` struct, it stores the format specifier values.
@@ -12,12 +11,41 @@ int check_buf(char *buf, int *buf_index);
  */
 int handle_c(char *buf, int *buf_index, fs_t *fs, va_list ap)
 {
-	int total_bytes_written = 0;
+	int total_bytes_written = 0, ch;
+	char padding_ch = ' ';
 
-	(void)buf;
-	(void)buf_index;
-	(void)fs;
-	(void)ap;
+	ch = va_arg(ap, int);
+
+	/*
+	 * Padding by ' ' before character,
+	 *  if The `flag_minus` turn off (default/normal case)
+	 */
+	if (!fs->flags.flag_minus)
+		while ((fs->width - 1) > 0)
+		{
+			total_bytes_written += _check_buf(buf, buf_index);
+			buf[*buf_index + 1] = padding_ch;
+			(*buf_index)++;
+			fs->width--;
+		}
+
+	/* Store the character in buffer(`buf`) */
+	total_bytes_written += _check_buf(buf, buf_index);
+	buf[*buf_index + 1] = ch;
+	(*buf_index)++;
+	fs->width--;
+
+	/*
+	 * Padding by ' ' after character,
+	 *  if The `flag_minus` turn on
+	 */
+	while (fs->width > 0)
+	{
+		total_bytes_written += _check_buf(buf, buf_index);
+		buf[*buf_index + 1] = padding_ch;
+		(*buf_index)++;
+		fs->width--;
+	}
 
 	return (total_bytes_written);
 }
---
+++ test/1-main_v1-handle_c.c
@@ -0,0 +1,39 @@
+#include "../main.h"
+
+int main(void)
+{
+	_printf("My _printf\n");
+	_printf("=========\n");
+
+	_printf("0.n:123456789\n");
+	_printf("1.n:%#-+0 5c$\n", 'a');
+	_printf("2.n:% #5c$\n", 'b');
+	_printf("3.n:%#5c$\n", 'c');
+	_printf("4.n:%+#7c$\n", 'd');
+	_printf("5.n:%# 5c$\n", 'e');
+	_printf("6.n:%#-+0 5.3c$\n", 'f');
+	_printf("7.n:%#-+0 .5c$\n", 'g');
+	_printf("8.n:%#-+0 0.5c$\n", 'h');
+	_printf("9.n:%c$\n", 65);
+	_printf("10.n:%c$\n", -5);
+	_printf("11.n:%c$\n", 200);
+
+	/* =========================================== */
+	_printf("The C _printf\n");
+	_printf("=========\n");
+
+	printf("0.n:123456789\n");
+	printf("1.n:%#-+0 5c$\n", 'a');
+	printf("2.n:% #5c$\n", 'b');
+	printf("3.n:%#5c$\n", 'c');
+	printf("4.n:%+#7c$\n", 'd');
+	printf("5.n:%# 5c$\n", 'e');
+	printf("6.n:%#-+0 5.3c$\n", 'f');
+	printf("7.n:%#-+0 .5c$\n", 'g');
+	printf("8.n:%#-+0 0.5c$\n", 'h');
+	printf("9.n:%c$\n", 65);
+	printf("10.n:%c$\n", -5);
+	printf("11.n:%c$\n", 200);
+
+	return (0);
+}
